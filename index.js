3a43667b32543527e5956430305c296b825c97d6
sichengzhao-coder <sicheng.zhao@bravechristian.org>
2026-05-26 09:25:21 -0700

    Update index.js

--- index.js
+++ index.js
@@ -1,16 +1,3 @@
-function setup() {
-    let canvas = createCanvas(600, 400);
-    canvas.parent("canvas-container");
-    background(0);
-}
+、console.log("Persona 5 website loaded.");
 
-function draw() {
-    fill(random(255), random(255), random(255));
-    noStroke();
-
-    circle(mouseX, mouseY, 20);
-}
-
-function changeColor(){
-    background(random(255), random(255), random(255));
-}
+alert("Welcome to my Persona 5 website!");
